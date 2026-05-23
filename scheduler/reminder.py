2f46935096e6d4917ce0f76281a959e8e9deca94
Damir <ogyrchik289@gmail.com>
2026-05-23 13:43:02 +0500

    fixed reminder

--- scheduler/reminder.py
+++ scheduler/reminder.py
@@ -7,9 +7,10 @@ async def check_deadlines(bot):
     for task in tasks:
         if task.status == "Выполнено":
             continue
-        parsed_deadline = datetime.datetime.strptime(task.deadline, "%Y-%m-%d" )
+        parsed_deadline = datetime.datetime.strptime(task.deadline, "%Y-%m-%d. %H:%M" )
         diff = parsed_deadline - now_time
-        if 0 < diff.total_seconds() / 3600 <= 24: 
+        hours_left = diff.total_seconds() / 3600
+        if 0 < hours_left <= task.remind_before:
             await bot.send_message(
                 chat_id = task.user_id,
                 text=f"⚠️ Задача '{task.title}' должна быть выполнена до {task.deadline}!")
